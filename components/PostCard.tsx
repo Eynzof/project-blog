9539871e265beaf40821a106f2bb32ba13d01e32
Eynzo Li <Eynzof@gmail.com>
2022-12-11 12:34:26 +0800

    basic components

---
+++ components/PostCard.tsx
@@ -0,0 +1,13 @@
+//@ts-nocheck
+import React from 'react';
+
+const PostCard = ({post}) => {
+    return (
+        <div>
+            {post.title}
+            {post.excerpt}
+        </div>
+    );
+};
+
+export default PostCard;
